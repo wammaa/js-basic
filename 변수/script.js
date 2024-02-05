36e052a7cfac74fba948b920c43f7ae3f3db2b71
Hwiho Lee <lhh323@gmail.com>
2024-02-05 16:24:58 +0900

    let var const

---
+++ 변수/script.js
@@ -0,0 +1,77 @@
+let a = 1
+let b = 2
+console.log(a,b)
+let c = a
+a = b
+b = c
+console.log(a,b)
+
+
+let d = 20+30
+console.log(d)
+
+let e = "20"+"30"
+console.log(e)
+
+let f = "Hello"+""+2021
+console.log(f)
+
+let g = 1+2*3
+console.log(g)
+
+let h = (1+3)**2
+console.log(h)
+
+let i = 1/0
+console.log(i)
+
+let j = 6%2
+console.log(j)
+
+let k = 7.5%2
+console.log(k)
+
+let l = 5==5
+console.log(l)
+
+let m = 5===5
+console.log(m)
+
+let n = 5=="5"
+console.log(n)
+
+let o = 5==="5"
+console.log(o)
+
+let p = 5!=5.0
+console.log(p)
+
+let q = 5!==5.0
+console.log(q)
+
+let r = "true"===true
+console.log(r)
+
+let s = 5<=5.0
+console.log(s)
+
+let t = 5>=5
+console.log(t)
+
+let u = true||true
+console.log(u)
+
+let v = true||false
+console.log(v)
+
+let w = true&&true
+console.log(w)
+
+let x = true&&false
+console.log(x)
+
+let y = !true
+console.log(y)
+
+let z = !false
+console.log(z)
