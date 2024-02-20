7c3375cdf7182890f68b9b2295c4888243343025
Hwiho Lee <lhh323@gmail.com>
2024-02-20 20:06:50 +0900

    2/20

---
+++ news/main.js
@@ -0,0 +1,12 @@
+const API_KEY = `ba6cff8a0208485f8e5d2cdcbf812b9a`;
+let news = [];
+
+const getLatestNews = async() =>{
+  const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+  const response = await fetch(url);
+  const data = await response.json();
+  news = data.articles;
+  console.log("rrrr", news);
+}
+
+getLatestNews();
